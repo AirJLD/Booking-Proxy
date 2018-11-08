18e6183229b6f4f962478a0cbcc22e035e750a93
Joshua Marx <marx.joshua@gmail.com>
2018-11-07 19:21:25 -0800

    updated server to include proxy routing for API calls

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const morgan = require('morgan');
+const proxy = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+const port = 3000;
+const publicDir = path.join(__dirname, 'public');
+
+const options = {
+  target: 'http://localhost:3000',
+  changeOrigin: true,
+  router: {
+    '/rooms': 'http://localhost:3004',
+    '/listings': 'http://localhost:1128',
+  },
+};
+
+const apiProxy = proxy(options);
+
+app.use('/rooms', apiProxy);
+app.use('/listings', apiProxy);
+
+app.use(express.static(publicDir));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
